9b9df23bed608311dd8935b63afb7498c7de88fd
Travis Athougies <travis@athougies.net>
2018-11-25 10:11:33 -0800

    Build multi-platform manifests

--- nix/build-bundle.nix
+++ nix/build-bundle.nix
@@ -1,5 +1,8 @@
-{ kite-app-module, system ? builtins.currentSystem, pkgs ? (import <nixpkgs> {})
-, pure-build ? false }:
+let defaultSystems = builtins.listToAttrs [ { name = builtins.currentSystem;
+                                              value = import <nixpkgs> {}; } ];
+in { kite-app-module
+   , systems ? defaultSystems
+   , pure-build ? false }:
 
 let kite-config = { config, pkgs, lib, ... }: {
       imports = [ ./kite.nix (builtins.toPath kite-app-module) ];
@@ -23,12 +26,30 @@ let kite-config = { config, pkgs, lib, ... }: {
       };
     };
 
-    eval = import <nixpkgs/nixos/lib/eval-config.nix> {
+    evalInPlatform = system: pkgs: import <nixpkgs/nixos/lib/eval-config.nix> {
       inherit system pkgs;
       modules = [ ./modules/top-level.nix (builtins.toPath kite-app-module) ];
       extraArgs = { kite-lib = (import ./lib/kite.nix) pkgs; inherit pure-build; };
     };
 
-    root = eval.config.kite.toplevel;
-in eval.config.kite.manifest
+    platforms = map (name: rec { inherit name; config = (evalInPlatform name (builtins.getAttr name systems)).config; package = config.kite.toplevel; })
+                    (builtins.attrNames systems);
+
+    config = (builtins.head platforms).config;
+    closures = builtins.listToAttrs (map ({name, package, ...}: { inherit name; value = package; }) platforms);
+
+in ((import <nixpkgs> {}).writeText "${config.kite.meta.slug}-manifest"
+    (builtins.toJSON {
+        name = config.kite.meta.name;
+        app-url = config.kite.meta.app-url;
+        icon = config.kite.meta.icon;
+#        authors = config.kite.meta.authors;
+        domain = config.kite.identifier;
+        nix-closure = closures;
+        runAsAdmin = config.kite.runAsAdmin;
+        singleton = config.kite.singleton;
+
+        bind-mounts = config.kite.bindMounts;
+      })) // { toplevels = closures; }
+
 
