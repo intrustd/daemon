a62df9eecee6b6a390fd29f68d01f23e35db8c86
Travis Athougies <travis@athougies.net>
2018-11-25 09:29:55 -0800

    Cross-compile apps for all supported systems

--- nix/build-hydra.nix
+++ nix/build-hydra.nix
@@ -1,12 +1,22 @@
-rec {
+let pkgs = import <nixpkgs> {};
+in with pkgs.lib;
 
-  manifest = import ./build-bundle.nix rec {
-    pkgs = import <nixpkgs> {};
-    system = pkgs.stdenv.targetPlatform.system;
-    kite-app-module = <src/kite.nix>;
-    pure-build = true;
-  };
+let systems = builtins.attrNames (import <system/systems.nix>);
 
-  app = manifest.toplevel;
+    mkJobs = platform:
+      let pkgs = (import <system/kite-appliance.nix> {
+                    inherit platform;
+                    nixpkgs-path = <nixpkgs>;
+                  }).pkgs;
 
-}
+          manifest = import ./build-bundle.nix rec {
+                       inherit pkgs;
+                       system = pkgs.stdenv.targetPlatform.system;
+                       kite-app-module = <src/kite.nix>;
+                       pure-build = true;
+                     };
+
+      in [ (nameValuePair "manifest-${platform}" manifest)
+           (nameValuePair "app-${platform}" manifest.toplevel) ];
+
+in listToAttrs (concatLists (map mkJobs systems))
